06edd77840cb4c481f7e86492c13bcf5684f7674
Robert <githubshiv@gmail.com>
2025-03-21 07:36:50 +0530

    Update play.py

--- AnieXEricaMusic/utils/inline/play.py
+++ AnieXEricaMusic/utils/inline/play.py
@@ -72,7 +72,7 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
-        InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
          InlineKeyboardButton(text="ᴇɴᴅ 🍁", callback_data=f"ADMIN Stop|{chat_id}"),
     ]
     return buttons
@@ -91,7 +91,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="ꜱᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="ꜱᴛᴏᴘ", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
          InlineKeyboardButton(text="ᴇɴᴅ 🍁", callback_data=f"ADMIN Stop|{chat_id}"),
     ]
     return buttons
